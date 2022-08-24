d2cce7f45e1357ea249b7cce3c8a6b716e59e0ff
WojciechWesolowski <wesolowski.wojciechck@gmail.com>
2022-08-24 23:16:02 +0200

    functions+-*/

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am not calculator, yet....");
-    }
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Variable 1:");
+        double x =scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("Variable 2:");
+        double y =scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("Choose +,-,*,/");
+        String choise = scan.nextLine();
+
+        if(choise.equals("+")){
+            System.out.println(x + y);
+        }if(choise.equals("-")){
+            System.out.println(x - y);
+        }if(choise.equals("*")){
+            System.out.println(x * y);
+        }if(choise.equals("/")){
+            System.out.println(x / y);
+        }
+
+
+    }
 }
