20b41344469e98dc9a4797b3fad7d560d0c93b2b
WojciechWesolowski <wesolowski.wojciechck@gmail.com>
2022-08-24 22:42:03 +0200

    fix.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am not calculator, yet...");
+        System.out.println("I am not calculator, yet....");
     }
 
 }
