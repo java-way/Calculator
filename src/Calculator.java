7f96657e38f8ac645b1b8e9891e6189f24904256
WojciechWesolowski <wesolowski.wojciechck@gmail.com>
2022-08-25 21:34:53 +0200

    2 of 3 fixed

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,33 +2,35 @@ package src;
 
 import java.util.Scanner;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
         System.out.println("I am not calculator, yet....");
 
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Variable 1:");
-        double x =scan.nextDouble();
+        double x = scan.nextDouble();
         scan.nextLine();
 
         System.out.println("Variable 2:");
-        double y =scan.nextDouble();
+        double y = scan.nextDouble();
         scan.nextLine();
 
         System.out.println("Choose +,-,*,/");
         String choise = scan.nextLine();
 
-        if(choise.equals("+")){
+        if (choise.equals("+")) {
             System.out.println(x + y);
-        }if(choise.equals("-")){
+        }
+        if (choise.equals("-")) {
             System.out.println(x - y);
-        }if(choise.equals("*")){
+        }
+        if (choise.equals("*")) {
             System.out.println(x * y);
-        }if(choise.equals("/")){
+        }
+        if (choise.equals("/")) {
             System.out.println(x / y);
         }
-
 
     }
 }
